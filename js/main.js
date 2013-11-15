019c42199ee20e34fbe6e615c4635081ddc8ae83
Tarick <gth766q@gatech.edu>
2013-11-15 16:42:13 -0500

    added terrain

--- js/main.js
+++ js/main.js
@@ -1,16 +1,9 @@
+//----- WORLD -----//
+
 var clock = new THREE.Clock();
 
 var angularSpeed = 0.2; 
 var lastTime = 0;
-
-var controls = {
-
-  moveForward: false,
-  moveBackward: false,
-  moveLeft: false,
-  moveRight: false
-
-};
 
 // set the scene size
 var WIDTH = window.innerWidth,
@@ -31,16 +24,27 @@ var camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR );
 var cameraControls = new THREE.OrbitControls( camera );
 var scene = new THREE.Scene();
 
-var keyboard = new THREEx.KeyboardState();
+// minecraft blocks
+var terrainMesh, worldWidth = 128, worldDepth = 128,
+      worldHalfWidth = worldWidth / 2, worldHalfDepth = worldDepth / 2,
+      data = generateHeight( worldWidth, worldDepth );
 
 //----- PLAYER -----//
-var player = new player();
 
-// key events
+var player = new player();
+var controls = {
+
+  moveForward: false,
+  moveBackward: false,
+  moveLeft: false,
+  moveRight: false
+
+};
 player.controls = controls;
 
+var keyboard = new THREEx.KeyboardState();
+
 // create a new mesh with geometry or import a model, find skin, assign to player
-//player.setMaterial('assets/images/crate.jpg');
 var sphere = new THREE.Mesh(new THREE.SphereGeometry(50, 16, 16), player.material);
 var loader = new THREE.ColladaLoader();
 loader.load('assets/models/steve_big.dae', function (result) {
@@ -114,11 +118,13 @@ function init() {
   scene.add( new THREE.AmbientLight( 0x222222 ) );
 
   var light = new THREE.DirectionalLight( 0xffffff, 2.25 );
-  light.position.set( 200, 450, 500, 1000 );
+  light.position.set( 1, 1, 0.5 ).normalize();
   light.castShadow = true;
   scene.add(light);
 
   //----- GROUND -----//
+  createTerrain();
+  /*
   var gt = THREE.ImageUtils.loadTexture( "assets/images/block_grass_128.jpg" );
   var gg = new THREE.PlaneGeometry( 16384, 16384 );
   var gm = new THREE.MeshPhongMaterial( { color: 0xffffff, map: gt } );
@@ -130,9 +136,157 @@ function init() {
   ground.receiveShadow = true;
 
   scene.add( ground );
+  */
 
   //----- CAMERA -----//
   cameraControls.addEventListener( 'change', function() {
     renderer.render(scene, camera);
   } );
+}
+
+//----- METHODS -----//
+
+function createTerrain() {
+
+  // code from threejs.org webgl geometry minecraft example
+  var matrix = new THREE.Matrix4();
+
+  var pxGeometry = new THREE.PlaneGeometry( 100, 100 );
+  pxGeometry.faceVertexUvs[ 0 ][ 0 ][ 0 ].y = 0.5;
+  pxGeometry.faceVertexUvs[ 0 ][ 0 ][ 2 ].y = 0.5;
+  pxGeometry.faceVertexUvs[ 0 ][ 1 ][ 2 ].y = 0.5;
+  pxGeometry.applyMatrix( matrix.makeRotationY( Math.PI / 2 ) );
+  pxGeometry.applyMatrix( matrix.makeTranslation( 50, 0, 0 ) );
+
+  var nxGeometry = new THREE.PlaneGeometry( 100, 100 );
+  nxGeometry.faceVertexUvs[ 0 ][ 0 ][ 0 ].y = 0.5;
+  nxGeometry.faceVertexUvs[ 0 ][ 0 ][ 2 ].y = 0.5;
+  nxGeometry.faceVertexUvs[ 0 ][ 1 ][ 2 ].y = 0.5;
+  nxGeometry.applyMatrix( matrix.makeRotationY( - Math.PI / 2 ) );
+  nxGeometry.applyMatrix( matrix.makeTranslation( - 50, 0, 0 ) );
+
+  var pyGeometry = new THREE.PlaneGeometry( 100, 100 );
+  pyGeometry.faceVertexUvs[ 0 ][ 0 ][ 1 ].y = 0.5;
+  pyGeometry.faceVertexUvs[ 0 ][ 1 ][ 0 ].y = 0.5;
+  pyGeometry.faceVertexUvs[ 0 ][ 1 ][ 1 ].y = 0.5;
+  pyGeometry.applyMatrix( matrix.makeRotationX( - Math.PI / 2 ) );
+  pyGeometry.applyMatrix( matrix.makeTranslation( 0, 50, 0 ) );
+
+  var pzGeometry = new THREE.PlaneGeometry( 100, 100 );
+  pzGeometry.faceVertexUvs[ 0 ][ 0 ][ 0 ].y = 0.5;
+  pzGeometry.faceVertexUvs[ 0 ][ 0 ][ 2 ].y = 0.5;
+  pzGeometry.faceVertexUvs[ 0 ][ 1 ][ 2 ].y = 0.5;
+  pzGeometry.applyMatrix( matrix.makeTranslation( 0, 0, 50 ) );
+
+  var nzGeometry = new THREE.PlaneGeometry( 100, 100 );
+  nzGeometry.faceVertexUvs[ 0 ][ 0 ][ 0 ].y = 0.5;
+  nzGeometry.faceVertexUvs[ 0 ][ 0 ][ 2 ].y = 0.5;
+  nzGeometry.faceVertexUvs[ 0 ][ 1 ][ 2 ].y = 0.5;
+  nzGeometry.applyMatrix( matrix.makeRotationY( Math.PI ) );
+  nzGeometry.applyMatrix( matrix.makeTranslation( 0, 0, -50 ) );
+
+  //
+
+  var geometry = new THREE.Geometry();
+  var dummy = new THREE.Mesh();
+
+  for ( var z = 0; z < worldDepth; z ++ ) {
+
+    for ( var x = 0; x < worldWidth; x ++ ) {
+
+      var h = getY( x, z );
+
+      dummy.position.x = x * 100 - worldHalfWidth * 100;
+      dummy.position.y = h * 100;
+      dummy.position.z = z * 100 - worldHalfDepth * 100;
+
+      var px = getY( x + 1, z );
+      var nx = getY( x - 1, z );
+      var pz = getY( x, z + 1 );
+      var nz = getY( x, z - 1 );
+
+      dummy.geometry = pyGeometry;
+      THREE.GeometryUtils.merge( geometry, dummy );
+
+      if ( ( px != h && px != h + 1 ) || x == 0 ) {
+
+        dummy.geometry = pxGeometry;
+        THREE.GeometryUtils.merge( geometry, dummy );
+
+      }
+
+      if ( ( nx != h && nx != h + 1 ) || x == worldWidth - 1 ) {
+
+        dummy.geometry = nxGeometry;
+        THREE.GeometryUtils.merge( geometry, dummy );
+
+      }
+
+      if ( ( pz != h && pz != h + 1 ) || z == worldDepth - 1 ) {
+
+        dummy.geometry = pzGeometry;
+        THREE.GeometryUtils.merge( geometry, dummy );
+
+      }
+
+      if ( ( nz != h && nz != h + 1 ) || z == 0 ) {
+
+        dummy.geometry = nzGeometry;
+        THREE.GeometryUtils.merge( geometry, dummy );
+
+      }
+
+    }
+
+  }
+
+  var texture = THREE.ImageUtils.loadTexture( 'assets/images/atlas.png' );
+  texture.magFilter = THREE.NearestFilter;
+  texture.minFilter = THREE.LinearMipMapLinearFilter;
+
+  var terrainMesh = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { map: texture, ambient: 0xbbbbbb } ) );
+  scene.add( terrainMesh );
+}
+
+//following utils from threejs.org examples
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+  cameraControls.handleResize();
+
+}
+
+function generateHeight( width, height ) {
+
+  var data = [], perlin = new ImprovedNoise(),
+  size = width * height, quality = 2, z = Math.random() * 100;
+
+  for ( var j = 0; j < 4; j ++ ) {
+
+    if ( j == 0 ) for ( var i = 0; i < size; i ++ ) data[ i ] = 0;
+
+    for ( var i = 0; i < size; i ++ ) {
+
+      var x = i % width, y = ( i / width ) | 0;
+      data[ i ] += perlin.noise( x / quality, y / quality, z ) * quality;
+
+
+    }
+
+    quality *= 4
+
+  }
+
+  return data;
+
+}
+
+function getY( x, z ) {
+
+  return ( data[ x + z * worldWidth ] * 0.2 ) | 0;
+
 }
